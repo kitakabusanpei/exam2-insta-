e3447f737bdf43ffb21c33958bb77af9ac99c216
渡邉江平 <kouhei.w.56@gmail.com>
2017-05-26 16:19:18 +0900

    first commit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,53 @@
+class PicturesController < ApplicationController
+  before_action :set_piuture, only: [:edit, :update, :destroy]
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    if params[:back]
+      @picture = Picture.new(pictures_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def create
+    @picture = Picture.new(pictures_params)
+    if @picture.save
+      redirect_to pictures_path, notice: "写真を投稿しました！" #indexに飛ばす
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @picture.update(pictures_params)
+      redilect_to pictures_path, notice: "写真を更新しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    ridilect_to pictures_path, notice: "写真を削除しました!"
+  end
+
+  def confirm
+    @picture = Picture.new(pictures_params)
+    render :new if @picture.invalid?
+  end
+
+  private
+  def pictures_params
+    params.require(:picture).permit(:title, :content, :image)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
+end
---
+++ app/models/picture.rb
@@ -0,0 +1,5 @@
+class Picture < ActiveRecord::Base
+  #  validates :title, presence: true
+  #  validates :content, presence: true
+  #  validates :image, presence: true
+end

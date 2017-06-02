f94c1f6bba3d3926ec69d72c141ef544e75a72be
渡邉江平 <kouhei.w.56@gmail.com>
2017-06-02 14:38:42 +0900

    login-function

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_piuture, only: [:edit, :update, :destroy]
   def index
     @pictures = Picture.all
